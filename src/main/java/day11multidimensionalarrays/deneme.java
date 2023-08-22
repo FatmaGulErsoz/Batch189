6ba91867465cafe270f51c94e3ab0e1211469549
FatmaGulErsoz <fatmagulersoz1412@gmai.com>
2023-08-22 23:20:36 +0300

    22/08/2023

---
+++ src/main/java/day11multidimensionalarrays/deneme.java
@@ -0,0 +1,23 @@
+package day11multidimensionalarrays;
+
+import java.util.ArrayList;
+
+public class deneme {
+    public static void main(String[] args) {
+
+
+        /*
+        1) Array'ler primitive data typleri ve referansları depolayabilir ama Arraylistler non-primitive'leri depolara
+        2) ArrayListler memory de Arraylerden daha fazla yer kaplar
+        3) Arraylar başlatırlırken baştan eleman sayisi belirtmek zorundayız
+        4) ArrayListler eleman sayısına esnektirler.
+        5) Arraylari java niye iptal etemdi:
+            a) Arraylar super hızlıdır
+            b) Arraylar memoryde çok az yer kaplar
+            c) Bu nedenle eleman sayısı belli olan dataları deplamak için Arraylar tercih edilir.
+         */
+
+        //ArrayList nasıl oluşturulur?
+        ArrayList<Integer> ages = new ArrayList<>();
+    }
+}
