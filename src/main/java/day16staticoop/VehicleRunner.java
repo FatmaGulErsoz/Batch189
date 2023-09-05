f611642b7337f26a19be5dd4b725bdb69810e006
FatmaGulErsoz <fatmagulersoz1412@gmail.com>
2023-09-05 22:54:29 +0300

    05/09/2023

--- src/main/java/day16staticoop/VehicleRunner.java
+++ src/main/java/day16staticoop/VehicleRunner.java
@@ -1,5 +1,7 @@
 package day16staticoop;
 
+import day17oop.Honda;
+
 public class VehicleRunner {
     public static void main(String[] args) {
         Honda civic = new Honda();
--- src/main/java/day18oop/StudentRunner.java
+++ src/main/java/day18oop/StudentRunner.java
@@ -1,2 +1,10 @@
-package day18oop;public class StudentRunner {
+package day18oop;
+
+public class StudentRunner {
+    public static void main(String[] args) {
+        Student ali = new Student();
+        System.out.println(ali.getStdId());//AC1234567
+        System.out.println(ali.getNotOrt());//3.87
+        System.out.println(ali.isSuccesful());//false
+    }
 }
